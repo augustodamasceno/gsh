d9d65cfd7f2f56bffb1120e5b3f4c45e79e1251c
Augusto Matheus <augustomatheuss@iMac-de-sergio.local>
2015-08-07 22:10:35 -0300

    testes2

--- showGitBranch.sh
+++ showGitBranch.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 # Show git branch on
 
-echo $( git branch | grep \* | tail -c $(wc -c $(git branch | grep \*)) )
+#echo $( git branch | grep \* | tail -c $(wc -c $(git branch | grep \*)) )
+names()
+{
+  names=($@)
+  echo ${names[@]:$#-1}
 
-#PS1=$PSI%showGitBranch
+}
+
+names $( git branch | grep \*)
+
+PS1=$(names $( git branch | grep \*)) - $PS1
