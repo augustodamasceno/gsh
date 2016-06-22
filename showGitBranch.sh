67a85ab5326b6a61e6e91ccfb416083e45262a91
Augusto Damasceno <augustomatheuss@dca.ufrn.br>
2016-06-22 10:32:19 -0300

    Update showGitBranch.sh

--- showGitBranch.sh
+++ showGitBranch.sh
@@ -7,3 +7,7 @@ OLDPS1=$PS1
 
 # CMD
 PS1='\u@\h \W$(__git_ps1 " (%s)")\$ '
+
+## FEDORA
+# source /usr/share/git-core/contrib/completion/git-prompt.sh
+# export PS1='[\u@\h \W$(declare -F __git_ps1 &>/dev/null && __git_ps1 " (%s)")]\$ '
