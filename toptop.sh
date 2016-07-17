2644db9374b0a7fcb4839dd27f1d7d8c4a5b4758
Augusto Damasceno <augustomatheuss@dca.ufrn.br>
2016-07-17 13:02:02 -0300

    Update toptop.sh

--- toptop.sh
+++ toptop.sh
@@ -3,6 +3,7 @@
 
 alias_toptop()
 {
+	printf '\033[0;31m----------BEGIN MONITOR--------\033[0m\n'
 	printf '\033[0;31m----------TOP CPU USE--------\033[0m\n'
 	ps aux | head -1
 	ps aux | sort -rn -k 3,6 | head -3
