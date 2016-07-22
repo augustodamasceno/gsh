a37444a664fb3d72bd7d33368000be01c36af4f4
Augusto Damasceno <augustomatheuss@dca.ufrn.br>
2016-07-22 10:29:05 -0300

    add prefix

---
+++ renamePre.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ADD Prefix to All Files in Current Folder
+# The user must confirm
+# sh renamePre.sh the-prefix
+
+
+pre=$1
+if [ ${#1} -gt 0 ]
+then
+	files=*
+
+	userMsg1='Rename all files in this folder adding the prefix \042'
+	echo -n $userMsg1$pre'\042 (y/N) '
+	read ok
+	
+	if [ "$ok" = "Y" ]; then ok="y";echo 'change';fi
+	if [ "$ok" = "y" ]
+	then
+		for i in *
+		do
+			newname=$pre$i
+			echo "renaming $i to $newname"
+			mv $i $newname
+		done
+	fi
+fi
+
