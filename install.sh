68db7ad7ac76f4dd5aa4de6892e43063cef34fdc
augustomatheuss <augustomatheuss@dca.ufrn.br>
2016-11-22 16:34:56 -0300

    not install the commands twice

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-echo "#------Scripts4bash-------------" >> ~/.bashrc
-cat cmd/cd.cmd >> ~/.bashrc
-cat cmd/showGitBranch.cmd  >> ~/.bashrc
-cat cmd/markhere.cmd >> ~/.bashrc
-cat cmd/toptop.cmd >> ~/.bashrc
-cat cmd/airkiss.cmd >> ~/.bashrc
-cat cmd/getip.cmd >> ~/.bashrc
-cat cmd/namekill.cmd >> ~/.bashrc
-cat cmd/getnames.cmd >> ~/.bashrc
-echo "#------Scripts4bash-------------" >> ~/.bashrc
+inst=$(cat .bashrc | grep Scripts4bash | wc -l)
+if [ "$inst" -eq "0" ]
+then
+	echo "#------Scripts4bash-------------" >> ~/.bashrc
+	cat cmd/cd.cmd >> ~/.bashrc
+	cat cmd/showGitBranch.cmd  >> ~/.bashrc
+	cat cmd/markhere.cmd >> ~/.bashrc
+	cat cmd/toptop.cmd >> ~/.bashrc
+	cat cmd/airkiss.cmd >> ~/.bashrc
+	cat cmd/getip.cmd >> ~/.bashrc
+	cat cmd/namekill.cmd >> ~/.bashrc
+	cat cmd/getnames.cmd >> ~/.bashrc
+	echo "#------Scripts4bash-------------" >> ~/.bashrc
+else
+	echo "Scripts4bash already installed. Remove previous commands first."
+fi 
+
